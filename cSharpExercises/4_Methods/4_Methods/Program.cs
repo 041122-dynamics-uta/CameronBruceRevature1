dfca29e8e97755b60f4854d9f8198528ba2fdb9d
Cameron Revis Bruce <103287170+CameronBruceRevature@users.noreply.github.com>
2022-05-01 14:46:40 -0700

    modified:   Program.cs

--- cSharpExercises/4_Methods/4_Methods/Program.cs
+++ cSharpExercises/4_Methods/4_Methods/Program.cs
@@ -13,28 +13,46 @@ namespace _4_MethodsChallenge
 
         public static string GetName()
         {
-            Console.
+            Console.WriteLine("What is your Name?");
+            string name = Console.ReadLine();
+            return name;
         }
 
         public static string GreetFriend(string name)
         {
-            throw new NotImplementedException("GreetFriend() is not implemented yet");
+            return "Hello, " + name + ". " + "You are my friend.";
         }
 
         public static double GetNumber()
         {
-            throw new NotImplementedException("GetNumber() is not implemented yet");
-
+            Console.WriteLine("What Is Your Number");
+            double number = Convert.ToDouble(Console.ReadLine());
+            return number;
         }
 
         public static int GetAction()
         {
-            throw new NotImplementedException("GetAction() is not implemented yet");
+            Console.WriteLine("What Is your Action");
+            int action = Convert.ToInt32(Console.ReadLine());
+            return action;
         }
 
         public static double DoAction(double x, double y, int action)
         {
-            throw new NotImplementedException("DoAction() is not implemented yet");
+            switch (action)
+            {
+                case 1:
+                    return x + y;
+                case 2:
+                    return Math.Abs(x - y);
+                case 3:
+                    return x * y;                   
+                case 4:
+                    return x / y; 
+                default:
+                    throw new FormatException("Wrong Format Type");
+                    break;
+            }   
         }
     }
 }
