caf4ab86d6407c4eefc8a245727f0ca5cd9069b2
Cameron Revis Bruce <103287170+CameronBruceRevature@users.noreply.github.com>
2022-05-12 19:32:00 -0700

    modified:   p1StoreApp/Program.cs

--- p1StoreApplication1/p1StoreApp/Program.cs
+++ p1StoreApplication1/p1StoreApp/Program.cs
@@ -16,6 +16,9 @@ namespace p1StoreApplication1
             //While Loop to innitialize program
             
             bool loopContinue = true;
+            bool admin = false;
+            bool existingCustomer = false;
+            bool newCustomer = false;
             while (loopContinue == true)
             {
 
@@ -31,22 +34,31 @@ namespace p1StoreApplication1
                     switch (choice)
                     {
                         case 1:
+                            Console.WriteLine("Please Enter Admin Varify ");
+                            int adminVerify = Convert.ToInt32(Console.ReadLine());
+                            
+                            if(adminVerify == 12345)
+                             {
+                                 admin = true;
+                             }
+                            else
+                            {
+                                Console.WriteLine();
+                            }
                         //Choice 1 enters into Admin Interface
                            //Admin UN and P Verify
-                           //Admin Interface Enters and Updates Store Information
-                           //Store location info, Product List add store Inventory info
+                           
                            
                         break;
                         case 2:
                         //Choice 2 enters into Existing Customer Interface
                             //Existing Customer UN and P Verify
-                            //Existing Customer Selects store Location, Selects product from inventory list, views/edits cart, and order history.
+                            
                             
                         break;
                         case 3:
                         //Choice 3 enters into New Customer Interface
-                            //Enters Customer Information into Database populates Various content in cart and Order History
-                            //Creates Customer UN and P for Future Verify
+                            
                             
                         break;
 
@@ -54,15 +66,26 @@ namespace p1StoreApplication1
                         case 4:
 
                         //Exits program
-                        loopContinue = !true;
+                        loopContinue = false;
                         break;
                     }
-                    if(loopContinue)
-                    Console.WriteLine("Please enter a valid choice.");
+                    
                 }
-            
-            
-            
+                while(admin == true)
+                {
+                    //Admin Interface Enters and Updates Store Information
+                           //Store location info, Product List add store Inventory info
+
+                }
+                while (existingCustomer == true)
+                {
+                    //Existing Customer Selects store Location, Selects product from inventory list, views/edits cart, and order history.
+                }
+                while (newCustomer == true)
+                {
+                    //Enters Customer Information into Database populates Various content in cart and Order History
+                            //Creates Customer UN and P for Future Verify
+                }
             
             //Console.WriteLine("Hello World!");
             // storeAppDomainClass sAD = new storeAppDomainClass();
